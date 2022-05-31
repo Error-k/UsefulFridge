0998bba4a4a3e1daf10d5ddbce0386b4f012b666
Egor Malinin <87609018+Error-k@users.noreply.github.com>
2022-05-31 22:33:26 +0500

    Delete query.php

--- query.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // $arr = $_POST['query'];
-    // $replacements = array(1 => "cereals");
-    // $ccc = array_replace($arr, $replacements);
-
-    // echo json_encode(array('success' => 1));
-    // echo $_POST['query'];
-    // echo json_encode($_POST['query']);
-?>
